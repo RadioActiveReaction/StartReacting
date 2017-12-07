4e1df9974e058ef52707db95b52ec6f9c4dca6e9
Anvesh Kolluri <Anveshkumar@Anveshs-MacBook-Pro.local>
2017-12-06 22:43:42 -0600

    Touch button and raise the count

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,64 +1,59 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { Component } from 'react'
+import {
+  AppRegistry,
+  StyleSheet,
+  TouchableHighlight,
+  Text,
+  View,
+} from 'react-native'
 
 export default class App extends Component {
-  render() {
-    let pic = {
-      uri: 'https://i.pinimg.com/564x/fa/e8/f4/fae8f49da663d919fefe0250843c1979.jpg'
-    };
+  constructor(props) {
+    super(props)
+    this.state = { count: 0 }
+  }
+
+  onPress = () => {
+    this.setState({
+      count: this.state.count+1
+    })
+  }
+
+ render() {
     return (
       <View style={styles.container}>
-        <Text style={{fontWeight: 'bold', color: 'red', fontSize: 30}}> Sasuke </Text>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Text style={styles.bigblue}>He Inspires </Text>
-        <Blink text='Yes.. he is no great' />
-        <Blink text='But he do not believe in this life and karma' />
-        <Blink text='he dont give a fuck about this world' />
-      </View>  
-    );
-  }
-}
-
-
-class Blink extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {showText: true};
-
-    // Toggle the state every second
-    setInterval(() => {
-      this.setState(previousState => {
-        return { showText: !previousState.showText };
-      });
-    }, 1000);
-  }
-
-  render() {
-    let display = this.state.showText ? this.props.text : ' ';
-    return (
-      <Text style={styles.white}>{display}</Text>
-    );
+        <TouchableHighlight
+         style={styles.button}
+         onPress={this.onPress}
+        >
+         <Text> Touch Here </Text>
+        </TouchableHighlight>
+        <View style={[styles.countContainer]}>
+          <Text style={[styles.countText]}>
+            { this.state.count !== 0 ? this.state.count: null}
+          </Text>
+        </View>
+      </View>
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 10
   },
-  bigblue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 20,
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10
   },
-  red: {
-    color: 'red',
-    fontWeight: 'bold',
-    fontSize: 30,
+  countContainer: {
+    alignItems: 'center',
+    padding: 10
   },
-  white: {
-    color: 'white'
+  countText: {
+    color: '#FF00FF'
   }
-});
+})
